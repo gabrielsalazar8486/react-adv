a370229990cb1882fe7244b8b0b5287de0c7a41d
gabrielsalazar8486 <salazargabriel8486@gmail.com>
2023-06-02 08:44:06 -0400

    Lazy log childs

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,14 +5,12 @@ import {BrowserRouter, Navigate, NavLink, Route, Routes} from 'react-router-dom'
 import {lazy, Suspense} from "react";
 
 export const Navigation = () => {
-  const Lazy1 = lazy(() => /* webpackChunkName: "LazyPage1" */import("./../01-lazyload/pages/LazyPage1"))
-  const Lazy2 = lazy(() => /* webpackChunkName: "LazyPage2" */import("./../01-lazyload/pages/LazyPage2"))
-  const Lazy3 = lazy(() => /* webpackChunkName: "LazyPage3" */import("./../01-lazyload/pages/LazyPage3"))
+  const LazyLayout = lazy(() =>  import(/* webpackChunkName: "LazyPage1" */ "./../01-lazyload/layout/LazyLayout"))
+  const noLazy = lazy(() => import(/* webpackChunkName: "LazyPage2" */ "./../01-lazyload/pages/NoLazy"))
 
   const routes = [
-    {to: "/lazy1", path: "lazy1", name: "Lazy1", Component: Lazy1},
-    {to: "/lazy2", path: "lazy2", name: "Lazy2", Component: Lazy2},
-    {to: "/lazy3", path: "lazy3", name: "Lazy3", Component: Lazy3}
+    {to: "/lazyload/", path: "/lazyload/*", name: "Lazy Layout", Component: LazyLayout},
+    {to: "/lazy2", path: "lazy2", name: "No Lazy", Component: noLazy}
   ]
   return (
       <Suspense fallback={<>Loadding...</>} >
